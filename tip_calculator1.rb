cc24b92da9b19693def2112a7093df7bee677dc9
Mackenzie Child <mackenzie.child@gmail.com>
2014-01-30 22:41:39 -0700

    Fix pre-tax cost of meal puts

--- tip_calculator1.rb
+++ tip_calculator1.rb
@@ -8,10 +8,10 @@ def tip_calculator()
 	tip_value = meal_price * tip_percentage
 	total_cost = meal_price + tax_value + tip_value
 
-	puts "The pre-tax cost of your meal was " + sprintf("%#g", meal_with_tax)
-	puts "At 6%, tax for this meal is $#{tax_value}"
-	puts "For a 35% tip, you should leave $#{tip_value}"
-	puts "The grand total for this meal is then $#{total_cost}"
+	puts "The pre-tax cost of your meal was " + sprintf("%.2f", meal_price)
+	puts "At 6%, tax for this meal is " + sprintf("%.2f", tax_value)
+	puts "For a 35% tip, you should leave " + sprintf("%.2f", tip_value )
+	puts "The grand total for this meal is then " + sprintf("%.2f", total_cost)
 end
 
 puts tip_calculator
